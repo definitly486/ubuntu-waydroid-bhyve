d61cd49d302ca203d3896cf9256750625aed355b
Your Name <you@example.com>
2025-05-17 13:46:23 +0300

    новый файл:    git.sh 	новый файл:    start-waydroid.sh

---
+++ git.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+git add --all
+git add .
+git commit -n
+git push  https://github.com/definitly486/ubuntu-waydroid-bhyve.git
